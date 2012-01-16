eee15e4586dce326a57e83e2cc11014412a018fe
Paul Connolley <paul.connolley@gmail.com>
2012-01-16 22:47:07 +0000

    tmuxinator and cakephp updates

--- cakephp/cakephp.zsh
+++ cakephp/cakephp.zsh
@@ -1,25 +1,8 @@
-function setCakeProject() {
-    if [[ $TMUX != "" ]]; then
-        tmux_uid=${TMUX//[^0-9]/}
-        key="VIMCAKE$tmux_uid"
-    else;
-        key="VIM"
-    fi
-    export cakeVimServerKey="$key"
-    export cakeAppBase=$PWD
-    export cakeApp=$PWD/app
-    alias -r vim-project-server="vim --servername $cakeVimServerKey"
-    alias -r v="tmux select-pane -t 0; vim --servername $cakeVimServerKey --remote-silent"
-    if [[ $1 = "launchVim" ]]; then
-        vim --servername $cakeVimServerKey
-    fi
-}
-
 function cake() {
-    if [[ -a "$cakeAppBase/cake/lib/Cake/VERSION.txt" ]]; then
-        /usr/local/bin/php -c $cakeAppBase/../cgi/php.ini -q $cakeApp/Console/cake.php -working "$cakeApp" -root "$cakeAppBase" -app "$cakeApp" "$@"
-    elif [[ -a "$cakeAppBase/cake/cake/VERSION.txt" ]]; then
-        $cakeAppBase/cake/cake/console/cake $@ -app $cakeApp -working $cakeApp -root $cakeAppBase/cake -core $cakeAppBase/cake
+    if [[ -a "$app_root/cake/lib/Cake/VERSION.txt" ]]; then
+        /usr/local/bin/php -c $app_root/../cgi/php.ini -q $app/Console/cake.php -working "$app" -root "$app_root" -app "$app" "$@"
+    elif [[ -a "$app_root/cake/cake/VERSION.txt" ]]; then
+        $app_root/cake/cake/console/cake $@ -app $app -working $app -root $app_root/cake -core $app_root/cake
     else
         echo Failed to load cake console
     fi
