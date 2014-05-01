deea1fc0a6cab5958885073640ee5c8af429375f
Paul Connolley <paul.connolley@gmail.com>
2014-05-01 14:29:30 +0100

    BASE16ORZ

--- zsh/dircolors.zsh
+++ zsh/dircolors.zsh
@@ -1,1 +1,2 @@
-eval $(dircolors $HOME/.dotfiles/zsh/dircolors/badwolf.dircolors)
+#eval $(dircolors $HOME/.dotfiles/zsh/dircolors/badwolf.dircolors)
+eval $(dircolors $HOME/.dotfiles/zsh/dircolors/solarized.dircolors)
