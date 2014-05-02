e75e44e377c5d8076cb8ee3ed0e912758ff15076
Paul Connolley <paul.connolley@gmail.com>
2014-05-02 21:58:41 +0100

    Migrated hub script for fish

---
+++ hub/hub.fish
@@ -0,0 +1,7 @@
+if test -f "/usr/local/bin/hub"
+  alias git="hub"
+end
+
+if test -f "$HOME/bin/hub"
+  alias git="$HOME/bin/hub"
+end
--- hub/hub.zsh
+++
@@ -1,7 +0,0 @@
-if [[ -f "/usr/local/bin/hub" ]]; then
-    alias -r git="hub"
-fi
-
-if [[ -f "$HOME/bin/hub" ]]; then
-    alias -r git="$HOME/bin/hub"
-fi
