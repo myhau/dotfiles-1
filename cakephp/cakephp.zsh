f2e67e4b198b7324aaa8dc80329cd4755f1a70c6
Paul Connolley <paul.connolley@gmail.com>
2014-05-02 21:55:13 +0100

    Removed cakephp.

--- cakephp/cakephp.zsh
+++
@@ -1,9 +0,0 @@
-function cake() {
-    if [[ -a "$app_root/cake/lib/Cake/VERSION.txt" ]]; then
-        /usr/local/bin/php -c $app_root/../cgi/php.ini -q $app/Console/cake.php -working "$app" -root "$app_root" -app "$app" "$@"
-    elif [[ -a "$app_root/cake/cake/VERSION.txt" ]]; then
-        $app_root/cake/cake/console/cake $@ -app $app -working $app -root $app_root/cake -core $app_root/cake
-    else
-        echo Failed to load cake console
-    fi
-}
