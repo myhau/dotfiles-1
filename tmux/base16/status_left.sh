266837b25e00bdc3dc5b6584a6f7f3419abd353a
Paul Connolley <paul.connolley@gmail.com>
2014-05-02 21:52:35 +0100

    I WENT FISH. BYE BYE ZSH :(

--- tmux/base16/status_left.sh
+++ tmux/base16/status_left.sh
@@ -8,10 +8,10 @@ SEGMENT_SEPARATOR=''
 # rendering default background/foreground.
 prompt_segment() {
   local bg fg
-  [[ -n $1 ]] && bg="colour${chash[$1]}" || bg="%k"
-  [[ -n $2 ]] && fg="colour${chash[$2]}" || fg="%f"
+  [[ -n $1 ]] && bg="$1" || bg="%k"
+  [[ -n $2 ]] && fg="$2" || fg="%f"
   if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
-    echo -n " #[bg=$bg,fg=colour${chash[$CURRENT_BG]}]$SEGMENT_SEPARATOR#[fg=$fg] "
+    echo -n " #[bg=$bg,fg=$CURRENT_BG]$SEGMENT_SEPARATOR#[fg=$fg] "
   else
     echo -n "#[bg=$bg,fg=$fg] "
   fi
@@ -22,7 +22,7 @@ prompt_segment() {
 # End the prompt, closing any open segments
 prompt_end() {
   if [[ -n $CURRENT_BG ]]; then
-    echo -n " #[default,fg=colour${chash[$CURRENT_BG]}]$SEGMENT_SEPARATOR"
+    echo -n " #[default,fg=$CURRENT_BG]$SEGMENT_SEPARATOR"
   else
     echo -n ""
   fi
@@ -31,53 +31,17 @@ prompt_end() {
 }
 
 prompt_context() {
-  local user=`whoami`
-
-  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
-    prompt_segment blackgravel snow "❐ #S"
-  fi
-}
-
-declare -A chash
-chash[plain]='15'
-chash[snow]='15'
-chash[coal]='16'
-chash[brightgravel]='252'
-chash[lightgravel]='245'
-chash[gravel]='243'
-chash[mediumgravel]='241'
-chash[deepgravel]='238'
-chash[deepergravel]='236'
-chash[darkgravel]='235'
-chash[blackgravel]='233'
-chash[blackestgravel]='232'
-chash[dalespale]='221'
-chash[dirtyblonde]='222'
-chash[taffy]='196'
-chash[saltwatertaffy]='121'
-chash[tardis]='39'
-chash[orange]='214'
-chash[lime]='154'
-chash[dress]='211'
-chash[toffee]='137'
-chash[coffee]='173'
-chash[darkroast]='95'
-
-prompt_who() {
-    user=$(whoami)
-    prompt_segment dirtyblonde blackgravel "$user"
+  prompt_segment "#3c3c3c" "#ffffff" "❐ #S"
 }
 
 prompt_boxname() {
     name=$(box_name)
-    prompt_segment coffee blackgravel "$name"
+    prompt_segment "#dd9975" "#ffffff" "$name"
 }
 
 build_prompt() {
     RETVAL=$?
     prompt_context
-    prompt_who
-    prompt_boxname
     prompt_end
 }
 
