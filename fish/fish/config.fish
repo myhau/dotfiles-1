d40bcbc58e7ec4b19819936634cf539f2e262af1
Paul Connolley <paul.connolley@gmail.com>
2014-05-03 19:22:53 +0100

    FEESH - TODAY'S FISH IS TROUT A LA CREME

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -9,6 +9,7 @@ set fish_theme base16
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
+set fish_plugins rvm node brew
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -49,3 +50,6 @@ end
 if test -n "$TMUX"
     set -x TERM screen-256color
 end
+
+#MOTD
+set -x fish_greeting 
