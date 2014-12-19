fbbb30d022b60f5275972c7a0a495c951ed65ff0
Paul Connolley <paul.connolley@gmail.com>
2014-12-19 13:27:03 +0000

    UPDATES

--- grep/theme.zsh
+++ grep/theme.zsh
@@ -1,3 +1,3 @@
 TOMORROWNIGHT="mt=38;5;143:sl=:cx=:fn=38;5;139:ln=38;5;222:bn=38;5;143:se=38;5;222"
-export GREP_OPTIONS="--color=auto -n"
+export GREP_OPTIONS="--color=auto"
 export GREP_COLORS=$TOMORROWNIGHT
